6c7c0daf8cb5d0484282ea3e6c010a70fe74d705
Moritz Gellner <MoritzGellner@wireless-165-124-241-253.nuwlan.northwestern.edu>
2013-04-17 23:04:37 -0500

    conflict resolution

--- app/assets/javascripts/caesar.js
+++ app/assets/javascripts/caesar.js
@@ -155,28 +155,6 @@ $(document).ready(function() {
 		var day = getMonday(new Date()).getDate();
 					
 		for (var k in COURSE_LIST) {
-<<<<<<< HEAD
-			var course = COURSE_LIST[k][0]; //just use first course for now
-			
-			var year = new Date().getFullYear();
-      		var month = new Date().getMonth();
-      		var day = getMonday(new Date()).getDate();
-      		
-      		var days = [course.M, course.T, course.W, course.R, course.F];
-
-			/* Create Events */
-			for (var j = 0; j < 5; j++) {
-				if (days[j] == "t") {
-					var calEvent = { 
-						"unique_id" : course.unique_id,
-						"colorid" : colorCounter,
-						"start" : new Date(year + '-' + (month+1) + '-' + (day + j) + ' ' + course.start.match(/(\d+:\d+)(\w+)/)[1] + ' ' + course.start.match(/(\d+:\d+)(\w+)/)[2]),
-						"end" : new Date(year + '-' + (month+1) + '-' + (day + j) + ' ' + course.end.match(/(\d+:\d+)(\w+)/)[1] + ' ' + course.end.match(/(\d+:\d+)(\w+)/)[2]),
-						"title" : course.subject + " " + course.number + " " + course.title
-					};
-					// console.log(calEvent);
-					$("#calendar").weekCalendar("updateEvent", calEvent);					
-=======
 			for (var c in COURSE_LIST[k]) {
 				course = COURSE_LIST[k][c];
 				if (course.onoff == true) {
@@ -217,11 +195,10 @@ $(document).ready(function() {
 									$("#calendar").weekCalendar("updateEvent", calEvent);					
 								}
 							}
-							//break;
+							break;
 						}
 					}
-					//break;
->>>>>>> implemented "onoff" field, makeCalendar now draws only those classes and sections where onoff=true
+					break;
 				}
 			}
 			colorCounter++;
