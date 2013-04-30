9c4d6e98a8065c191d61ca9027de1bc2cf83858c
Al Johri <al.johri@gmail.com>
2013-04-29 19:43:47 -0500

    Fix for Safari and Firefox. Part 2.

--- app/assets/javascripts/caesar.js
+++ app/assets/javascripts/caesar.js
@@ -61,8 +61,8 @@ function makeCalendar() {
 								var calEvent = { 
 									"unique_id" : section.unique_id,
 									"colorid" : colorCounter,
-									"start" : new Date(year + '-' + (month+1) + '-' + (day + i) + ' ' + section.start.match(/(\d+:\d+)(\w+)/)[1] + ' ' + section.start.match(/(\d+:\d+)(\w+)/)[2]),
-									"end" : new Date(year + '-' + (month+1) + '-' + (day + i) + ' ' + section.end.match(/(\d+:\d+)(\w+)/)[1] + ' ' + section.end.match(/(\d+:\d+)(\w+)/)[2]),
+									"start" : new Date(year + '/' + (month+1) + '/' + (day + i) + ' ' + section.start.match(/(\d+:\d+)(\w+)/)[1] + ' ' + section.start.match(/(\d+:\d+)(\w+)/)[2]),
+									"end" : new Date(year + '/' + (month+1) + '/' + (day + i) + ' ' + section.end.match(/(\d+:\d+)(\w+)/)[1] + ' ' + section.end.match(/(\d+:\d+)(\w+)/)[2]),
 									"title" : section.subject + " " + section.number + " " + section.title
 								};
 								console.log(calEvent);
