ebee93c02d9e23b2894efa6047cb99cd0258d392
Al Johri <al.johri@gmail.com>
2013-05-13 14:06:00 -0500

    phonegap stuff

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ if defined?(Bundler)
   #Bundler.require(:default, :assets, Rails.env)
 end
 
-module Coursica
+module Courseseek
   class Application < Rails::Application
 
     config.generators do |g|
