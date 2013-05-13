91584094e53d05befb7db353ca118cf5fb4a76b9
Al Johri <al.johri@gmail.com>
2013-05-13 05:44:28 -0500

    Disabled sections accidently.

--- app/assets/javascripts/caesar.js
+++ app/assets/javascripts/caesar.js
@@ -40,7 +40,7 @@ function makeCalendar() {
 						var end_time = moment(course.end, "h:mmA");
 						var end = moment(monday).add('days', j).hours(end_time.hours()).minutes(end_time.minutes()).seconds(end_time.seconds());						
 
-						var calEvent = { 
+						var calEvent = {
 							"unique_id" : course.unique_id,
 							"colorid" : colorCounter,
 							"start" : start.toDate(),
@@ -63,10 +63,10 @@ function makeCalendar() {
 						var _days = [section.M, section.T, section.W, section.R, section.F];
 						for (var i = 0; i < 5; i++) {
 
-							var start_time = moment(course.start, "h:mmA");
+							var start_time = moment(section.end.start, "h:mmA");
 							var start = moment(monday).add('days', j).hours(start_time.hours()).minutes(start_time.minutes()).seconds(start_time.seconds());
 
-							var end_time = moment(course.end, "h:mmA");
+							var end_time = moment(section.end.end, "h:mmA");
 							var end = moment(monday).add('days', j).hours(end_time.hours()).minutes(end_time.minutes()).seconds(end_time.seconds());						
 
 							if (_days[i] == "t") {
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,20 @@ Coursica::Application.routes.draw do
   resources :users
   resources :sessions
   match ':controller(/:action(/:id(.:format)))'
-end
 
 # https://github.com/rails/rails/issues/10091#issuecomment-15917805
+# Put this at the end of your config/routes.rb
+# if Rails.env.development?
+#   app = ActionDispatch::Static.new(
+#     lambda{ |env| [404, { 'X-Cascade' => 'pass'}, []] },
+#     Rails.application.config.paths['public'].first,
+#     Rails.application.config.static_cache_control
+#   )
+
+#   mount app, :at => '/', :as => :public
+# end
+  
+end
+
+
+
