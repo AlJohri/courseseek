9133f54e6c8c988cff75dda3c12a0cd72f9d1708
Al Johri <al.johri@gmail.com>
2013-05-14 20:16:23 -0500

    Added bio classes.

--- app/jobs/course_job.rb
+++ app/jobs/course_job.rb
@@ -13,7 +13,6 @@ class CourseJob
 		login_form.set_fields(:userid => username) #ARGV[0]
 		login_form.set_fields(:pwd => password) #ARGV[1]
 		login_form.action = 'https://ses.ent.northwestern.edu/psp/caesar/?cmd=?languageCd=ENG'
-		pp login_form
 		page = @agent.submit(login_form, login_form.buttons.first)
 		@authenticated = true
 	end
@@ -23,12 +22,11 @@ class CourseJob
 		# 	authenticate
 		# end
 		authenticate
-
 		error_counter = 0
 
 		@subjects = Subject.all
     
-		@subjects.each { |row|
+		# @subjects.each { |row|
 
 			doc = @agent.get('https://ses.ent.northwestern.edu/psc/caesar_6/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL?Page=SSR_CLSRCH_ENTRY').parser
 			icsid = doc.xpath("//*[@id='ICSID']/@value").text
@@ -36,13 +34,13 @@ class CourseJob
 			icstatenum = doc.xpath("//*[@id='ICStateNum']/@value").text
 
 			institution = "NWUNV"
-			subject = row.subject #ARGV[0]
+			subject = "BIOL_SCI" #row.subject #ARGV[0]
 			match_type = "E"
 			career = "UGRD"
 			open_course_only = "N"
-			term = "4520" #"4490"
+			term = "4520"
 
-			ajax_headers = { 'Content-Type' => 'application/x-www-form-urlencoded; charset=utf-8'}
+			ajax_headers = { 'Content-Type' => 'application/x-www-form-urlencoded; charset=utf-8' }
 			params = {
 				"ICAction" => "CLASS_SRCH_WRK2_SSR_PB_CLASS_SRCH",
 				"ICSID" => icsid,
@@ -56,8 +54,28 @@ class CourseJob
 				"CLASS_SRCH_WRK2_SSR_EXACT_MATCH1" => match_type,
 				"CLASS_SRCH_WRK2_ACAD_CAREER" => career,
 				"CLASS_SRCH_WRK2_SSR_OPEN_ONLY$chk" => open_course_only,
-				"DERIVED_SSTSNAV_SSTS_MAIN_GOTO$152$"=>"9999"
+				"DERIVED_SSTSNAV_SSTS_MAIN_GOTO$152$"=>"9999",
 			}
+
+			if subject.include? ("BIOL_SCI")
+				params_bio = {
+					"CLASS_SRCH_WRK2_INCLUDE_CLASS_DAYS"=>"J",
+					"CLASS_SRCH_WRK2_MON$chk"=>"Y",
+					"CLASS_SRCH_WRK2_MON"=>"Y",
+					"CLASS_SRCH_WRK2_TUES$chk"=>"Y",
+					"CLASS_SRCH_WRK2_TUES"=>"Y",
+					"CLASS_SRCH_WRK2_WED$chk"=>"Y",
+					"CLASS_SRCH_WRK2_WED"=>"Y",
+					"CLASS_SRCH_WRK2_THURS$chk"=>"Y",
+					"CLASS_SRCH_WRK2_THURS"=>"Y",
+					"CLASS_SRCH_WRK2_FRI$chk"=>"Y",
+					"CLASS_SRCH_WRK2_FRI"=>"Y",
+					"CLASS_SRCH_WRK2_SAT$chk"=>"",
+					"CLASS_SRCH_WRK2_SUN$chk"=>""				
+				}
+
+				params = params.merge params_bio
+			end
 
 			response = @agent.post('https://ses.ent.northwestern.edu/psc/caesar_4/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL', params, ajax_headers)
 			doc  = Nokogiri::HTML(response.body)
@@ -69,44 +87,43 @@ class CourseJob
 			partsCounter2 = 0
 
       if (error.empty?)
-			  courses.each_with_index { |x,i| 
+
+				courses.each_with_index { |x,i| 
           courses[i] = CGI.unescapeHTML(courses[i].to_s).delete!("^\u{0000}-\u{007F}")
           courses[i] =~ /(^\w+)(\s+)(\d+-\d+) - (.*)/
-
           department = $1
           number = $3
           title = $4
-					parts = doc.search("div[id='win6div$ICField108GP$" + i.to_s + "'] > table > tr > td[2] > span[3]/text()").to_s.gsub(/1.*of\s/, "").to_i
+				  parts = doc.search("div[id='win6div$ICField108GP$" + i.to_s + "'] > table > tr > td[2] > span[3]/text()").to_s.gsub(/1.*of\s/, "").to_i
+				  checkEdgeCase = doc.search("div[id='win6divSSR_CLSRCH_MTG1$" + partsCounter1.to_s + "'] > table > tr")
 
-					checkEdgeCase = doc.search("div[id='win6divSSR_CLSRCH_MTG1$" + partsCounter1.to_s + "'] > table > tr")
+  				if (checkEdgeCase.length > 2)
+  					puts "ERROR EDGE CASE #{department} #{number}"
+  					partsCounter1 += checkEdgeCase.length - 1
+  					partsCounter2 += 1
+  					next
+  				end
 
-					if (checkEdgeCase.length > 2)
-						puts "ERROR EDGE CASE #{department} #{number}"
-						partsCounter1 += checkEdgeCase.length - 1
-						partsCounter2 += 1
-						next
-					end
+				  puts "--------------#{department} #{number} has #{parts} parts"
 
-					puts "--------------#{department} #{number} has #{parts} parts"
-
-          parts.times { |x|
-          	uniqueid_sec = doc.search("a[id='DERIVED_CLSRCH_SSR_CLASSNAME_LONG$" + partsCounter2.to_s + "']").text
+        	parts.times { |x|
+            uniqueid_sec = doc.search("a[id='DERIVED_CLSRCH_SSR_CLASSNAME_LONG$" + partsCounter2.to_s + "']").text
           	uniqueid_sec =~ /(\w+)-(\w+)\((\d+)\)/
           	section = $1
           	lecdisc = $2
           	unique_id = $3
 
-          	days_time = doc.search("span[id='MTG_DAYTIME$" + partsCounter1.to_s + "']").text
-          	if (days_time != "TBA")
-	          	days_time =~ /^(\w+) (\d\d?:\d\d(AM|PM)) - (\d\d?:\d\d(AM|PM))/
-						 	days = $1
-						 	start_time = $2
-						 	end_time = $4
-						else
-							days = "TBA"
-							start_time = "TBA"
-							end_time = "TBA"
-						end
+            days_time = doc.search("span[id='MTG_DAYTIME$" + partsCounter1.to_s + "']").text
+            if (days_time != "TBA")
+              days_time =~ /^(\w+) (\d\d?:\d\d(AM|PM)) - (\d\d?:\d\d(AM|PM))/
+    				  days = $1
+    				  start_time = $2
+    				  end_time = $4
+            else
+    				  days = "TBA"
+    				  start_time = "TBA"
+    				  end_time = "TBA"
+            end
 
           	room = doc.search("span[id='MTG_ROOM$" + partsCounter1.to_s + "']").text
           	instructor = doc.search("span[id='MTG_INSTR$" + partsCounter1.to_s + "']").text
@@ -147,8 +164,9 @@ class CourseJob
           	partsCounter1+=1
           	partsCounter2+=1
           }
-          
+
         }
+
       else
       	error = error.to_s
       	error = error.gsub("The search returns no results that match the criteria specified.", "No courses this quarter.")
@@ -163,7 +181,8 @@ class CourseJob
         	puts error#.red
         end
       end
-		}
+		# }
+
 	end
 
 	def backup_database()
