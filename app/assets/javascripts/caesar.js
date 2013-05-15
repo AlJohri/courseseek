81cbe3ca5cbf5c975c9b1011644e3c9e1acc4bea
Al Johri <al.johri@gmail.com>
2013-05-15 09:02:31 -0500

    STUPID AL 3

--- app/assets/javascripts/caesar.js
+++ app/assets/javascripts/caesar.js
@@ -375,7 +375,7 @@ $(document).ready(function() {
 
 		var url = decodeURIComponent(document.URL);
 		url = url.replace("http://www.courseseek.co/?uniqueID=", "");
-		url.replace(/\+/g, " ");
+		url = url.replace(/\+/g, " ");
 
 		if(url) {
 			courses = url.split(",");
